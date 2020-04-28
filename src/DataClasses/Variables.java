a244b4892981f0e45d938e2c87e38d283ace7294
kowlad <wladickowalew@gmail.com>
2020-04-28 18:02:48 +0300

    3 commit

--- src/DataClasses/Variables.java
+++ src/DataClasses/Variables.java
@@ -3,14 +3,14 @@ package DataClasses;
 public class Variables {
     // constants
     public static final int CELL_SIZE = 40;
-    public static final int FIELD_WIDTH = 800;
-    public static final int FIELD_HEIGHT = 600;
 
     public static final int WALLS_COUNT = 40;
 
     //variables
     public static int CELLS_W;
     public static int CELLS_H;
+    public static int FIELD_WIDTH;
+    public static int FIELD_HEIGHT;
 
     public static void computeValues(){
         CELLS_W = FIELD_WIDTH  / CELL_SIZE;

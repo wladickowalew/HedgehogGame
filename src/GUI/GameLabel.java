e131692a4b4b2406c5aa2226856b0891ab620a27
kowlad <wladickowalew@gmail.com>
2020-05-19 16:22:01 +0300

    add GameLabel class

---
+++ src/GUI/GameLabel.java
@@ -0,0 +1,27 @@
+package GUI;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GameLabel extends JLabel {
+
+    private String text;
+
+    public GameLabel(String text){
+        super(text);
+        setForeground(Color.WHITE);
+        Font f = new Font("Comic Sans MS", 0, 24);
+        setFont(f);
+        setSize(200,30);
+    }
+
+    public GameLabel(String text, int number){
+        super(text + ": " + number);
+        this.text = text;
+    }
+
+    public void change(int number){
+        setText(text + ": " + number);
+    }
+
+}
